228a94404c48db274154776a6476f23d67058e88
Vitaly Rtishchev <rtivital@gmail.com>
2017-01-20 12:47:38 +0300

    add library source

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const CASES = [2, 0, 1, 1, 1, 2];
+
+
+export default function proschet(titles) {
+  return function bindedProschet(amount) {
+    return titles[
+      (amount % 100 > 4 && amount % 100 < 20) ? 2 : CASES[(amount % 10 < 5) ? amount % 10 : 5]
+    ];
+  };
+}
